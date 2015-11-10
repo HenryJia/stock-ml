37d1f54a55e03f6b5048cb20e08d83cdc52f5b6d
Hengjian Jia <henryjia18@yahoo.com>
2015-11-10 23:49:26 +0000

    Adds extreme value learning to improve directional error to 45%.

--- lstm2.py
+++ lstm2.py
@@ -4,6 +4,7 @@ import os
 import time
 import csv
 import math
+import sys
 
 import theano
 import theano.tensor as T
@@ -12,20 +13,22 @@ from keras import optimizers
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Reshape, Flatten
 from keras.layers.embeddings import Embedding
-from keras.layers.recurrent import LSTM, SimpleRNN
+from keras.layers.recurrent import LSTM, SimpleRNN, SimpleDeepRNN, JZS3
 from keras.layers.convolutional import Convolution2D
 import matplotlib.pyplot as plt
+from matplotlib.finance import candlestick2_ohlc
 
 from get import get
 
 epsilon = 1
 
 n = 5
-days = 20
-batch_size = 10
-lstm1_units = 256
-lstm2_units = 512
-lstm3_units = 1
+batch_size = 100
+train_days = 30
+test_days = 30
+lstm1_units = 32
+lstm2_units = 32
+lstm3_units = 32
 dense_units = 512
 grad_clip = 100
 random.seed(0)
@@ -55,23 +58,41 @@ def normalize(data):
 
 def normalize_known(data, mean, std):
   result = (data - mean) / std
-  return result  
+  return result
+count = 0
+def get_next_extreme(data):
+  if (data[0, 3] <= data[0 + 1, 3]):
+    for i in range(len(data) - 1):
+      if (data[i, 3] > data[i + 1, 3]):
+        return data[i, :]
+  elif (data[0, 3] > data[0 + 1, 3]):
+    for i in range(len(data) - 1):
+      if (data[i, 3] < data[i + 1, 3]):
+        return data[i, :]
+  global count
+  count += 1
+  print 'Default used ', count, ' times'
+  return data[-1, :]
 
-def gen_data(data, m):
-  length = data.shape[0] - m - 1
+def gen_data(data, m, predict_date, extreme):
+  length = data.shape[0] - m - predict_date
   x = np.zeros((length, m, 5))
-  y = np.zeros((length, m, 5))
+  y = np.zeros((length, m, 4))
   y_dir = np.zeros(length)
   for i in range(length):
     x[i] = data[i:(i + m)]
-    y[i] = data[(i + 1):(i + m + 1)]
-    if (data[(i + m), 3] > data[(i + m - 1), 3]):
+    if (extreme == True):
+      for j in range(m):
+        y[i, j, :] = get_next_extreme(data[(i + j):, 0:4])
+    elif (extreme == False):
+      y[i] = data[(i + predict_date):(i + m + predict_date), 0:4]
+    if (data[(i + m), 3] > data[(i + m - predict_date), 3]):
         y_dir[i] = 1
     #y[i] = (data[(i + m), 3] - x_mean[3]) / x_std[3]
   return x, y, y_dir
 
 print 'Generating Data...'
-data = get('stock_data_clean/KAZ_LDX.TXT', -1, 100)
+data = get('stock_data_clean/KAZ_LDX.TXT', -1, 200)
 data_test = get('stock_data_clean/KAZ_LDX.TXT', 200, 1)
 
 data_np = np.array(data)
@@ -90,29 +111,33 @@ data_test_np[:, 4] += 1
 data_np = np.log(data_np)
 data_test_np = np.log(data_test_np)
 
-#data_np, mean_np, std_np = normalize(data_np)
-#data_test_np = normalize_known(data_test_np, mean_np, std_np)
+data_np, mean_np, std_np = normalize(data_np)
+data_test_np = normalize_known(data_test_np, mean_np, std_np)
 
-x, y, y_dir = gen_data(data_np, days)
-x_test, y_test, y_test_dir = gen_data(data_test_np, days)
+x, y, y_dir = gen_data(data_np, train_days, 0, True) # 3rd argument not ncesssary if true
+x_test, y_test, y_test_dir = gen_data(data_test_np, test_days, 1, False)
+print 'NaN Check x ', np.isnan(np.sum(x))
+print 'NaN Check y ', np.isnan(np.sum(y))
+if (np.isnan(np.sum(x)) or np.isnan(np.sum(y))):
+  sys.exit('Error NaN(s) detected.')
 
 print 'Building Model...'
 model = Sequential()
 #model.add(Reshape((1, days, n), input_shape=(days, n)))
 #model.add(Convolution2D(64, 5, 5))
 #model.add(Reshape((days - 5 + 1, 64)))
-model.add(LSTM(lstm1_units, return_sequences=True, activation='tanh', input_shape=(days, n)))
+model.add(LSTM(lstm1_units, return_sequences=True, activation='tanh', input_shape=(train_days, n)))
 model.add(Dropout(0.5))
-model.add(LSTM(lstm2_units, return_sequences=True))
+model.add(LSTM(lstm2_units, return_sequences=True, activation='tanh'))
 model.add(Dropout(0.5))
-#model.add(LSTM(lstm3_units, return_sequences=True))
-#model.add(Dropout(0.5))
-model.add(SimpleRNN(5, return_sequences=True, activation='linear'))
+model.add(LSTM(lstm3_units, return_sequences=True, activation='tanh'))
+model.add(Dropout(0.5))
+model.add(SimpleRNN(4, return_sequences=True, activation='linear'))
 sgd = optimizers.SGD(lr=0.1, decay=0, momentum=0.9, nesterov=True)
 adagrad = optimizers.adagrad(lr=0.1)
 adadelta = optimizers.Adadelta(lr=1.0)
 print 'Compiling...'
-model.compile(loss="mean_squared_error", optimizer=adadelta)
+model.compile(loss="mean_squared_error", optimizer='rmsprop')
 
 #if os.path.isfile('keras-weights.nn'):
   #print 'Loading Model...'
@@ -120,7 +145,6 @@ model.compile(loss="mean_squared_error", optimizer=adadelta)
 print 'Testing Model...'
 score = model.evaluate(x_test, y_test, batch_size=batch_size, verbose=1, show_accuracy=True)
 print 'Test Score: ', score
-#print 'Test Error: ', (math.sq
 print 'Begin Training...'
 model.fit(x, y, batch_size=batch_size, nb_epoch=epochs, validation_data=(x_test, y_test))#, validation_split=0.05)
 print 'Saving Model...'
@@ -146,8 +170,36 @@ percentage = wrong / float(len(result)) * 100
 print 'Test Direction Error: ', wrong, ' Out of ', len(result),  ' Percentage: ', percentage
 #for i in range(len(y_test)):
   #print 'Predictions: ', result[i] , ' Correct: ', (y_test[i] * std_np[3] + mean_np[3])
-plt.plot(np.exp(result[:, -1, 3]), 'bs')
-#plt.plot(np.exp(y_test[:, 3] * std_np[3] + mean_np[3]), 'r^')
-plt.plot(np.exp(y_test[:, -1, 3]), 'r^')
+#plt.plot(np.exp(result[:, -1, 3] * std_np[3] + mean_np[3]), 'bs')
+f, axarr = plt.subplots(2, sharey=True)
+recovered_result = np.exp(result[:, -1, :] * std_np[0:4] + mean_np[0:4])
+candlestick2_ohlc(axarr[0], recovered_result[:, 0], recovered_result[:, 1], recovered_result[:, 2], recovered_result[:, 3], width=1, colorup='g', colordown='r')
+#plt.plot(np.exp(result[:, -1, 3]), 'bs')
+recovered_y_test = np.exp(y_test[:, -1, :] * std_np[0:4] + mean_np[0:4])
+candlestick2_ohlc(axarr[1], recovered_y_test[:, 0], recovered_y_test[:, 1], recovered_y_test[:, 2], recovered_y_test[:, 3], width=1, colorup='g', colordown='r')
+#plt.plot(np.exp(y_test[:, -1, 3] * std_np[3] + mean_np[3]), 'r^')
+#plt.plot(np.exp(y_test[:, -1, 3]), 'r^')
+f, axarr = plt.subplots(2, sharey=True)
+result_x = model.predict(x, batch_size=batch_size, verbose=1)
+recovered_result_x = np.exp(result_x[-100:, -1, :] * std_np[0:4] + mean_np[0:4])
+
+result_dir_x = np.zeros(result_x.shape[0])
+for i in range(len(result_x)):
+  if (result_x[i, -1, 3] > x[i, -1, 3]):
+    result_dir_x[i] = 1
+
+
+wrong = 0
+for i in range(len(result_x)):
+  if (result_dir_x[i] != y_dir[i]):
+    wrong += 1
+
+percentage = wrong / float(len(result_dir_x)) * 100
+print 'Train Direction Error: ', wrong, ' Out of ', len(result_dir_x),  ' Percentage: ', percentage
+
+
+candlestick2_ohlc(axarr[0], recovered_result_x[:, 0], recovered_result_x[:, 1], recovered_result_x[:, 2], recovered_result_x[:, 3], width=1, colorup='g', colordown='r')
+recovered_y = np.exp(y[-100:, -1, :] * std_np[0:4] + mean_np[0:4])
+candlestick2_ohlc(axarr[1], recovered_y[:, 0], recovered_y[:, 1], recovered_y[:, 2], recovered_y[:, 3], width=1, colorup='g', colordown='r')
 plt.show()
 print 'All Done :D'
